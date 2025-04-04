c66a2a7e93a3644441d90a496ca70b75469367a6
JOSE HENRIQUE <carvalhocostajr@gmail.com>
2025-04-04 22:04:21 +0000

    aplição If

---
+++ aplicacao_if_nota.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int nota = 75;
+
+  if (nota >= 60){
+  printf("Parabéns Você passou!!\n");
+}
+}
---
+++ aplicacao_if_temperatura.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  
+    float temperatura = 30.0;
+
+    if (temperatura >= 30.0){
+    printf("Está Calor.\n");
+}    
+    printf("Do lado de fora!\n");
+}
