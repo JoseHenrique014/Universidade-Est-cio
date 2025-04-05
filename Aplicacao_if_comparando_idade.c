069fb2147d23d67c8d4fa82a91ff232998d286d2
JOSE HENRIQUE <carvalhocostajr@gmail.com>
2025-04-05 00:01:32 +0000

    COMPARAÇÃO IDADE

---
+++ Aplicacao_if_comparando_idade.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int idadeJose = 35;
+    int idadePaulo = 33;
+
+    if (idadeJose > idadePaulo){
+        printf("José é o mais velho\n");
+    
+    }
+    
+}
